abaf0664a2209a3171d73512c0cc3999ecde3c9c
Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
2016-09-23 21:04:31 +0200

    Ajout du lieu à la requête le cas échéant

--- routes/places.js
+++ routes/places.js
@@ -6,19 +6,40 @@ var Checks = require('../modules/checks');
 var Place = require('../models/place');
 
 router.param('id', Checks.isValidObjectId);
-router.param('id', placeExists);
+router.param('id', getPlace);
 
 router.get('/', Checks.db, getPlacesHeaders);
 router.get('/:id', Checks.db, getPlaceInfo);
 
 
-function placeExists(req, res, next, id) {
-  Place.findById(id, { "_id": true }, function onPlaceFound(err, place) {
+function getPlace(req, res, next, id) {
+  // Get the place without heavy content like comments, …
+  var projection = {
+    location: true,
+    title: true,
+    isVerified: true,
+    proposedBy: true,
+    type: true,
+    description: true,
+    startDate: true,
+    endDate: true,
+    manager: true,
+    moderateComments: true,
+    moderatePictures: true,
+    moderateDocuments: true,
+    denyComments: true,
+    denyPictures: true,
+    denyDocuments: true
+  };
+
+  Place.findById(id, projection, function onPlaceFound(err, place) {
     if (!place) {
       var err = new Error('Not Found');
       err.status = 404;
       return next(err);
     }
+
+    req.place = place
 
     next();
   });
