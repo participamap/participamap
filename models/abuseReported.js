e9f218ba1ab9072532db86564364a0f1c44b346f
Gwen <gwendadou@hotmail.fr>
2016-10-13 11:23:04 +0200

    createAbuseReported

--- models/abuseReported.js
+++ models/abuseReported.js
@@ -5,9 +5,9 @@ var abuseReportedSchema = new Schema({
   user: { type: Schema.Types.ObjectId, required: true },
   date: { type: Date, default: Date.now, required: true },
   contentReported: { type: Schema.Types.ObjectId, required: true },
-  content: { type: String,enum: ["comment","document","picture"], required: true }
+  type: { type: String, enum: ["comment","document","picture"], required: true }
 });
 
-var AbuseReportedSchema = mongoose.model('Comment', abuseReportedSchema);
+var AbuseReportedSchema = mongoose.model('AbuseReported', abuseReportedSchema);
 
 module.exports = AbuseReportedSchema;
