d224f0ba68afc955ffeb9032f322f1b7c94397d3
Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
2016-10-26 15:11:35 +0200

    Ajout d’un titre pour les documents

--- models/document.js
+++ models/document.js
@@ -9,6 +9,7 @@ var documentSchema = new Schema({
   place: { type: Schema.Types.ObjectId, required: true },
   author: { type: Schema.Types.ObjectId, required: true },
   date: { type: Date, default: Date.now, required: true },
+  title: { type: String, required: true },
   type: { type: String, required: true },
   url: { type: String, required: true },
   toModerate: Boolean
