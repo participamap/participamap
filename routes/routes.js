3d4cf12cb3e4d96f11a37ce9355c77855ceafeed
Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
2016-10-26 13:10:01 +0200

    Validation du code

--- routes/routes.js
+++ routes/routes.js
@@ -2,77 +2,148 @@ var express = require('express');
 var mongoose = require('mongoose');
 
 var Checks = require('../modules/checks');
-var Routes = require('../models/routes');
 var Utils = require('../modules/utils');
-var ObjectId = require('mongodb').ObjectId;
 
-var config = require('../config.json');
+var Route = require('../models/route');
 
-var router = express.Router();
+var ObjectId = mongoose.Types.ObjectId;
 
-router.param('id', getRoutes);
+var router = express.Router({ strict: true });
+
+router.param('id', Checks.isValidObjectId);
+router.param('id', getRoute);
+
+// getRoutesHeaders
+router.get('/',
+  Checks.db,
+  getRoutesHeaders);
+
+// getRouteInfo
+router.get('/:id',
+  getRouteInfo);
+
+// createRoute
+router.post('/',
+  Checks.auth('content-owner'),
+  Checks.db,
+  createRoute,
+  Utils.cleanEntityToSend(),
+  Utils.send);
+
+// updateRoute
+router.put('/:id',
+  Checks.auth('content-owner'),
+  updateRoute,
+  Utils.cleanEntityToSend(),
+  Utils.send);
+
+// TODO: addPlace
+//router.post('/:id/',
+//  Checks.auth('content-owner'),
+//  addPlace,
+//  Utils.cleanEntityToSend(),
+//  Utils.send);
+
+// TODO: removePlace
+//router.delete('/:id/place_id',
+//  Checks.auth('content-owner'),
+//  removePlace,
+//  Utils.cleanEntityToSend(),
+//  Utils.send);
+
+// deleteRoute
+router.delete('/:id',
+  Checks.auth('content-owner'),
+  deleteRoute);
 
 
-router.get('/',Checks.db,getRoutesHeaders);
-router.post('/', Checks.db, createRoutes);
-
-router.delete('/:id', Checks.db, deleteRoutes);
-
-function getRoutes(req, res, next, id) {
-  Routes.findById(id, { __v: false }, function onRoutesFound(error, routes) {
+function getRoute(req, res, next, id) {
+  Route.findById(id, { __v: false }, function onRouteFound(error, route) {
     if (error) return next(error);
 
-    if (!routes) {
+    if (!route) {
       var err = new Error('Not Found');
       err.status = 404;
       return next(err);
     }
 
-    req.routes = routes;
+    req.placesRoute = route;
 
     next();
   });
 }
 
-function getRoutesHeaders(req,res,next){
-	var filter = {};	
-	var projection = {
-		title: true,
-		places: true
-		
-	};
-	if(req.query.place){
-		if (!ObjectId.isValid(req.query.place)) {
-			var err = new Error('Bad Request: Invalid ID');
-    			err.status = 400;
-    			return next(err);
-  		}
-		filter = { places : req.query.place};
-	}
-	Routes.find(filter, projection,
-                function returnRoutesHeaders(error, routesHeaders) {
-                        if (error) return next(error);
-                        res.json(routesHeaders);
-                });
+
+function getRoutesHeaders(req, res, next) {
+  var filter = {};
+  
+  if (req.query.place) {
+    if (!ObjectId.isValid(req.query.place)) {
+      var err = new Error('Bad Request: place must be a valid ObjectId');
+      err.status = 400;
+      return next(err);
+    }
+
+    filter.places = req.query.place;
+  }
+  
+  var projection = {
+    title: true
+  };
+  
+  Route.find(filter, projection,
+    function returnRoutesHeaders(error, routesHeaders) {
+      if (error) return next(error);
+      res.json(routesHeaders);
+    });
 }
-function deleteRoutes(req, res, next) {
 
-  var routes = req.routes;
 
-  routes.remove(function onRoutesRemoved(error) {
+function getRouteInfo(req, res, next) {
+  res.json(req.placesRoute);
+}
+
+
+function createRoute(req, res, next) {
+  var route = new Route({
+    title: req.body.title,
+    places: req.body.places
+  });
+
+  // TODO: Un lieu ne peut pas être deux fois dans une route
+  // TODO: Un lieu doit exister pour être dans une route
+
+  var onRouteSaved = Utils.returnSavedEntity(req, res, next, 201);
+  route.save(onRouteSaved);
+}
+
+
+function updateRoute(req, res, next) {
+  var route = req.placesRoute;
+  var modifications = req.body;
+
+  // Delete unchangeable attributes
+  delete modifications._id;
+  delete modifications.__v;
+
+  // TODO: Un lieu ne peut pas être deux fois dans une route
+  // TODO: Un lieu doit exister pour être dans une route
+
+  for (attribute in modifications)
+    route[attribute] = modifications[attribute];
+
+  var onRouteSaved = Utils.returnSavedEntity(req, res, next);
+  route.save(onRouteSaved);
+}
+
+
+function deleteRoute(req, res, next) {
+  var route = req.placesRoute;
+
+  route.remove(function onRouteRemoved(error) {
     if (error) return next(error);
     res.status(204).end();
   });
-}
-
-
-function createRoutes(req, res, next) {
-  var routes = req.routes;
-
-  var routes = new Routes(req.body);
-
-  var onRoutesSaved = Utils.returnSavedEntity(res, next, 201);
-  routes.save(onRoutesSaved);
 }
 
 
