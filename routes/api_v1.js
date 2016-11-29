6b9c8d954119a918d340761737b9b8f548f21d33
Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
2016-11-29 21:03:26 +0100

    Ajout d’une limite du nombre d’appels API
    Closes #36

--- routes/api_v1.js
+++ routes/api_v1.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+var RateLimit = require('express-rate-limit');
 
 var Auth = require('../modules/auth');
 
@@ -12,12 +13,24 @@ var routes = require('./api_v1/routes');
 var abuseReports = require('./api_v1/abuse_reports');
 var upload = require('./api_v1/upload');
 
+var config = require('../config.json');
+
 var router = express.Router({ strict: true });
+
+// Rate limiter
+var apiLimiter = new RateLimit({
+  windowMs: config.rateLimiter.windowSize * 1000,
+  max: config.rateLimiter.max,
+  delayMs: 0,
+});
 
 // Passport for authentication
 passport.use(new LocalStrategy(Auth.verify));
 
 // Modules
+router.use(apiLimiter);
+router.use(passport.initialize());
+router.use(Auth.jwt);
 router.use(bodyParser.json());
 router.use(bodyParser.raw({ type: 'image/jpeg', limit: '5MB' }));
 router.use(bodyParser.raw({ type: 'image/png', limit: '5MB' }));
@@ -26,8 +39,6 @@ router.use(bodyParser.raw({ type: 'application/pdf', limit: '10MB' }));
 router.use(bodyParser.raw({ type: 'application/msword', limit: '5MB' }));
 router.use(bodyParser.raw({ type: 'application/vnd.oasis.opendocument.text',
   limit: '5MB' }));
-router.use(passport.initialize());
-router.use(Auth.jwt);
 
 // Routes
 router.use('/', index);
